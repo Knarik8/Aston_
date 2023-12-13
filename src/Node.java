efa6aee3e7cdf79a64c725a9c1d2c6777c14eb1e
Knarik8 <k_armina@mail.ru>
2023-12-14 00:00:10 +0400

    Initial commit

---
+++ src/Node.java
@@ -0,0 +1,14 @@
+public class Node<T> {
+    T value;
+    Node<T> nextNode; //reference for next node
+
+    Node(T value, Node<T> nextNode) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+
+    Node(T value){ //using when we don't know nextNode and prevNode
+        this.value = value;
+    }
+}
+
